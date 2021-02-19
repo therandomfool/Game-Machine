545345f06c4553d28df5c4d60d887fac4f4f4761
therandomfool <mddenver@protonmail.com>
2021-02-18 20:47:06 -0800

    Button functions . added JS

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById("concentration").onclick = function () {
+    location.href = "www.yoursite.com";
+};
+
+
+document.getElementById("chess").onclick = function () {
+    location.href = "https://distracted-blackwell-f5a415.netlify.app/";
+};
+
+
+document.getElementById("mastermind").onclick = function () {
+    location.href = "https://elastic-engelbart-96af88.netlify.app/" ;
+};
